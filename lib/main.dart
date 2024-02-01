e75786caf2bc7ae0ca9852c0319dfc2e0ae21044
rrendryo <rrendryo@gmail.com>
2024-02-01 17:52:54 +0700

    add cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,32 +14,92 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void tambahData() {
+    emit(state + 1);
+  }
+
+  void kurangData() {
+    emit(state - 1);
+  }
+}
+
+// ignore: must_be_immutable
 class homePage extends StatelessWidget {
+  CounterCubit mycounter = CounterCubit();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BloC - Cubit'),
+        title: Center(
+          child: Text(
+            'BloC - Cubit',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
         backgroundColor: Colors.amberAccent,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text('0'),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.minimize),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder(
+              stream: mycounter.stream,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: Text(
+                      'loading...',
+                      style: TextStyle(
+                        fontSize: 100,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: Text(
+                      "${snapshot.data}",
+                      style: TextStyle(
+                        fontSize: 100,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }
+              }),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  mycounter.kurangData();
+                },
+                icon: Icon(
+                  Icons.remove,
+                  color: Colors.white,
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.maximize),
+              ),
+              IconButton(
+                onPressed: () {
+                  mycounter.tambahData();
+                },
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
